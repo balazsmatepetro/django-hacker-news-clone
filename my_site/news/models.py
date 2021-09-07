bd90f2e1be421666b2dd2f88b3938016e3b7164f
balazsmatepetro <petrobalazsmate@gmail.com>
2021-09-07 16:07:55 +0200

    feat(news): add base `NewsItem` model and migration

--- my_site/news/models.py
+++ my_site/news/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import Truncator
 
-# Create your models here.
+
+class NewsItem(models.Model):
+    title = models.CharField(max_length=256)
+    url = models.URLField(max_length=2000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} ({Truncator(text=self.url).chars(num=64)})'
+
