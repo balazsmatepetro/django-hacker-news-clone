1cc9485fa86701f5d2d581ea236058cd4f42e9da
balazsmatepetro <petrobalazsmate@gmail.com>
2021-09-08 16:46:44 +0200

    feat(accounts): add basic authentication functionality

--- my_site/accounts/urls.py
+++ my_site/accounts/urls.py
@@ -6,6 +6,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
 ]
--- my_site/accounts/views.py
+++ my_site/accounts/views.py
@@ -1,13 +1,23 @@
+from django.contrib.auth.views import LoginView as BaseLoginView, LogoutView as BaseLogoutView
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .forms import LoginForm
+
+
+class LoginView(BaseLoginView):
+    form_class = LoginForm
+    template_name = 'accounts/login.html'
+    redirect_authenticated_user = True
+
+
+class LogoutView(BaseLogoutView):
+    """TODO: Maybe I could use the LoginRequiredMixin here."""
+    template_name = 'accounts/logout.html'
 
 
 def index(request):
     return HttpResponse('This is the profile page.')
-
-
-def login(request):
-    return HttpResponse('This is the login page.')
 
 
 def register(request):
