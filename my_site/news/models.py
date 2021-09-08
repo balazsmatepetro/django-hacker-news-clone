fcc9684faa8c0b298bc475b1bd06cb7f5460f1ff
balazsmatepetro <petrobalazsmate@gmail.com>
2021-09-08 09:06:36 +0200

    feat(news): show the number of comments in case of the news item

--- my_site/news/models.py
+++ my_site/news/models.py
@@ -9,6 +9,9 @@ class NewsItem(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def get_number_of_comments(self):
+        return self.comment_set.count()
+
     def __str__(self):
         return f'{self.title} ({Truncator(text=self.url).chars(num=64)})'
 
