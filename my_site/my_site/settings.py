be80463c1ca4f3f6b605bd00e9f08f34e04f53dd
balazsmatepetro <petrobalazsmate@gmail.com>
2021-09-09 10:54:32 +0200

    refactor(site): move layout template under the project templates

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'my_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            Path(__file__).resolve().parent / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
